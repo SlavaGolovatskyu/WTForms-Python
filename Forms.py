f4e5e7aae9ce46fdd914be32967ec8fb54400dee
Slava <61500332+SlavaGolovatskyu@users.noreply.github.com>
2021-03-07 21:53:09 +0200

    Update Forms.py

--- Forms.py
+++ Forms.py
@@ -2,7 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
-""" 
+"""  
+	WTForms
+	WTForms – это мощная библиотека, написанная на Python и независимая от фреймворков. Она умеет генерировать формы,
+	проверять их и предварительно заполнять информацией (удобно для редактирования) и многое другое. 
+	Также она предлагает защиту от CSRF. Для установки WTForms используется Flask-WTF.
+
+	Flask- WTF – это расширение для Flask, которое интегрирует WTForms во Flask. Оно также предлагает дополнительные функции,
+	такие как загрузка файлов, reCAPTCHA, интернационализация (i18n) и другие. Для установки Flask-WTF нужно ввести следующую команду
+	
+	
 	Пакет wtform предлагает несколько классов, представляющих собой следующие поля:
 	StringField, PasswordField, SelectField, TextAreaField, SubmitField
 """
