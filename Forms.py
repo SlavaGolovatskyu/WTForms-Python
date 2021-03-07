bbf16ce00411446ca60c31cdd15fb3104544bc27
Slava <61500332+SlavaGolovatskyu@users.noreply.github.com>
2021-03-07 21:54:55 +0200

    Update Forms.py

--- Forms.py
+++ Forms.py
@@ -1,5 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import (
+    StringField,
+    SubmitField,
+    TextAreaField
+)
+
 from wtforms.validators import DataRequired, Email
 
 """  
