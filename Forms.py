d0aea177d2a62f2ad552364425bcb230d8bfe5e3
Slava <61500332+SlavaGolovatskyu@users.noreply.github.com>
2021-03-07 21:52:16 +0200

    Update Forms.py

--- Forms.py
+++ Forms.py
@@ -2,6 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
+""" 
+	Пакет wtform предлагает несколько классов, представляющих собой следующие поля:
+	StringField, PasswordField, SelectField, TextAreaField, SubmitField
+"""
+
 class ContactForm(FlaskForm):
 	name = StringField("Name: ", validators=[DataRequired()])
 	email = StringField("Email: ", validators=[Email()])
